f16b58bfca21311c996b5bbae005cc076cbd5d25
AlienBeat <109912046+LissetheGarcia@users.noreply.github.com>
2025-05-02 19:00:30 -0600

    Delete CodeCoverage/SimpleStartExample directory

--- CodeCoverage/SimpleStartExample/build8/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lissethe/CMakeT1
-# Build directory: /home/lissethe/CMakeT1/build8
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
