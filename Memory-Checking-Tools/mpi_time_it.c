2cff709fd04f05da39ba1259b647f3c9db8f17cd
alienBeat <lissethe.777@gmail.com>
2025-05-16 19:19:17 -0600

    MPI c program for running with valgrind memcheck tool

---
+++ Memory-Checking-Tools/mpi_time_it.c
@@ -0,0 +1,38 @@
+// Program using a timer to produce small differences in output to compare serial and parallel output
+// using mpi library for C/C++
+// mpi compile: mpicc -g mpi_time_it.c -o mpi_time_it
+// valgrind compile: mpirun -n 2 valgrind --leak-check=full --show-leak-kinds=all --track_origins=yes --log-file="mpi_valgrind_memcheck.log" ./mpi_time_it
+// Set var to avoid mpi warnings: PMIX_MCA_pcompress_base_silence_warning=1
+// PMIX_MCA_pcompress_base_silence_warning=1
+
+// Header for access to functions to the POSIX API of the OS, useful for kernel interaction
+#include <unistd.h>
+#include <stdio.h>
+// Header for performing parallel programming in C
+#include <mpi.h>
+
+int main(int argc, char *argv[])
+{
+    int mype;
+
+    // MPI initialization and processor rank spawn
+    // This function initiliazes MPI environment, taking addresses from argc and argv command line args.
+    MPI_Init(&argc, &argv);
+    // Manage communicators for process communicators
+    /* this function returns the process ID of the processor that called the function.
+     * takes the MPI environment and the memory address of an integer value */
+    MPI_Comm_rank(MPI_COMM_WORLD, &mype);
+
+    double t1, t2;
+
+    t1 = MPI_Wtime();
+    sleep(10);
+    t2 = MPI_Wtime();
+    if(mype == 0)
+    {
+        printf("Elapsed time is %f secs\n", t2-t1);
+    }
+
+    // This function cleans up MPI environment & ends MPI communications
+    MPI_Finalize();
+}
